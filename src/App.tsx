a004bafe6868655caa00bcb891ca76a847aabf7f
Phelippe Duarte Ambrosio <14623@ait.nsw.edu.au>
2026-05-21 10:25:24 +1000

    feat: implement user state management and integrate sign-up functionality in Drawer

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,22 @@ import "./App.css";
 import Navigation from "./components/Navigation";
 import Drawer from "./components/Drawer";
 import { DrawerProvider } from "./context/DrawerProvider";
+import { useState } from "react";
+import type { User } from "./types/user";
 
 function App() {
+  const [user, setUser] = useState<User | undefined>(undefined);
+
+  function handleSignUp(newUser: User) {
+    setUser(newUser);
+  }
+
   return (
     <DrawerProvider>
       <main className="App">
         <Navigation navigation="home" />
       </main>
-      <Drawer />
+      <Drawer user={user} onSignUp={handleSignUp} />
     </DrawerProvider>
   );
 }
